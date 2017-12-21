243df8f1678eed4de7b2dcc8563cad22bb611dcb
certainly <j1o2b3@gmail.com>
2017-12-21 16:19:17 +0800

    ! coddle

--- PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
@@ -1,7 +1,53 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var str = "Hell222o, playground"
+struct Photo: Codable
+{
+    //String, URL, Bool and Date conform to Codable.
+    var title: String
+    var url: URL
+    var isSample: Bool
+    
+    //The Dictionary is of type [String:String] and String already conforms to Codable.
+    var metaData: [String:String]
+    
+    //PhotoType and Size are also Codable types
+    var type: PhotoType
+    var size: Size
+}
 
-//: [Next](@next)
+struct Size: Codable
+{
+    var height: Double
+    var width: Double
+}
+
+enum PhotoType: String, Codable
+{
+    case flower
+    case animal
+    case fruit
+    case vegetable
+}
+
+let photoObject = Photo(title: "Hibiscus", url: URL(string: "https://www.flowers.com/hibiscus")!, isSample: false, metaData: ["color" : "red"], type: .flower, size: Size(height: 200, width: 200))
+let encodedData = try? JSONEncoder().encode(photoObject)
+
+let jsonString = """
+{
+"type":"fruit",
+"size":{
+"width":150,
+"height":150
+},
+"title":"Apple",
+"url":"https:\\/\\/www.fruits.com\\/apple",
+"isSample":true,
+"metaData":{
+"color":"green"
+}
+}
+"""
+if let jsonData = jsonString.data(using: .utf8)
+{
+    let photoObject = try? JSONDecoder().decode(Photo.self, from: jsonData)
+}
