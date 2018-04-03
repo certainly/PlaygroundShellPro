69e16011d25c1a55de0da0197b507103c8d131f7
certainly <j1o2b3@gmail.com>
2018-04-03 10:49:45 +0800

    - quicksort

--- PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
@@ -1,1 +1,27 @@
-print("oki")
+func quickSort_theFunctional(_ array: [Int]) -> [Int] {
+
+    guard array.count > 1 else {
+        return array
+    }
+
+    let (pivot, rest) = (array.first!, array.dropFirst())
+
+    let lessThan = rest.filter{ $0 < pivot }
+    let greaterThanOrEqual = rest.filter{ $0 >= pivot }
+
+    return quickSort_theFunctional(lessThan) + ([pivot] as [Int]) + quickSort_theFunctional(greaterThanOrEqual)
+}
+
+// Sorted
+quickSort_theFunctional([Int]()) == [Int]()
+quickSort_theFunctional([7]) == [7]
+quickSort_theFunctional([1, 1, 2, 3, 5, 8, 13]) == [1, 1, 2, 3, 5, 8, 13]
+
+// Nearly Sorted
+quickSort_theFunctional([1, 2, 7, 3, 5, 13, 8]) == [1,  2, 3, 5,7, 8, 13]
+
+// Reversed
+quickSort_theFunctional([1, 1, 2, 3, 5, 8, 13].reversed()) == [1, 1, 2, 3, 5, 8, 13]
+
+// Shuffled
+//quickSort_theFunctional([1, 1, 2, 3, 5, 8, 13].shuffled()) == [1, 1, 2, 3, 5, 8, 13]
