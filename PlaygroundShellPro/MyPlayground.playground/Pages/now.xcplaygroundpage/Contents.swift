0724c18b5ce6d448024016a4c4e327dfb0317fed
certainly <j1o2b3@gmail.com>
2018-05-19 15:48:17 +0800

    - hex to color

--- PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
@@ -1,28 +1,30 @@
-class Logger {
-    static let shared = Logger()
+import UIKit
 
-    private init() { }
+print("begin3438")
 
-    func log(_ message: String) {
-        print(message)
+
+
+func hexStringToUIColor (hex:String) -> UIColor {
+    var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+    if (cString.hasPrefix("#")) {
+        cString.remove(at: cString.startIndex)
     }
-}
 
-protocol Logging {
-    func log(_ message: String)
-}
-
-extension Logging {
-    func log(_ message: String) {
-        Logger.shared.log(message)
+    if ((cString.count) != 6) {
+        return UIColor.gray
     }
+
+    var rgbValue:UInt32 = 0
+    Scanner(string: cString).scanHexInt32(&rgbValue)
+
+    return UIColor(
+        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+        alpha: CGFloat(1.0)
+    )
 }
 
-struct MainScreen: Logging {
-    func authenticate() {
-        log("Authentication was successful!")
-    }
-}
 
-let screen = MainScreen()
-screen.authenticate()
+hexStringToUIColor(hex: "#e43511")
