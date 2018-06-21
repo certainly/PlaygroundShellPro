958e9f51c51227aad1c1d70d5edf72b6e53e48a9
certainly <j1o2b3@gmail.com>
2018-06-21 15:26:29 +0800

    - roshambo

--- PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
@@ -1,30 +1,7 @@
-import UIKit
-
-print("begin3438")
-
-
-
-func hexStringToUIColor (hex:String) -> UIColor {
-    var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-
-    if (cString.hasPrefix("#")) {
-        cString.remove(at: cString.startIndex)
-    }
-
-    if ((cString.count) != 6) {
-        return UIColor.gray
-    }
-
-    var rgbValue:UInt32 = 0
-    Scanner(string: cString).scanHexInt32(&rgbValue)
-
-    return UIColor(
-        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-        alpha: CGFloat(1.0)
-    )
+let roshamdo = ["rock", "paper", "scissors"]
+func check(_ left: String, beats right: String) -> Bool {
+    return (3 + roshamdo.firstIndex(of: left)! - roshamdo.firstIndex(of: right)! ) % 3 == 1
 }
+let computerMove = roshamdo.shuffled().first!
+check("paper", beats: computerMove)
 
-
-hexStringToUIColor(hex: "#e43511")
