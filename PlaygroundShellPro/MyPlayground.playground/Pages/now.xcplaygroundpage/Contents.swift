a001501c3049655dfcfcfa8feabdf53901ed5444
certainly <j1o2b3@gmail.com>
2018-07-12 17:23:17 +0800

    color rgb str

--- PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
@@ -1,20 +1,19 @@
 import UIKit
 import PlaygroundSupport
 
-extension URL: ExpressibleByStringLiteral {
-    public init(stringLiteral value: String) {
-        self = URL(string: value)!
+let ss = "202,203,206,1"
+
+ func getColorFromRGBStr(_ colorStr: String?) -> UIColor? {
+    guard colorStr != nil else {
+        return nil
     }
+    let tArrSplited = colorStr?.components(separatedBy: ",")
+    guard let tArr = tArrSplited, tArr.count == 4 else {
+        return nil
+    }
+    let tArrFloat = tArr.map{  CGFloat(( $0 as NSString).floatValue)}
+
+    return UIColor(red: (tArrFloat[0]) / 255.0, green: (tArrFloat[1]) / 255.0, blue: (tArrFloat[2]) / 255.0, alpha: tArrFloat[3])
 }
 
-var url:URL = "http://www.baidu.com"
-let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-    let ss = String(data: data!, encoding: .utf8)!
-    print(ss)
-    PlaygroundPage.current.finishExecution()
-}
-
-task.resume()
-
-PlaygroundPage.current.needsIndefiniteExecution = true
-
+getColorFromRGBStr(ss)
