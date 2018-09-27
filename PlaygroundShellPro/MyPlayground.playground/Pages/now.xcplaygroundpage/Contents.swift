1dc024444f8c2dafb1d50e2b47b8cdb5c2159150
certainly <j1o2b3@gmail.com>
2018-09-27 12:09:54 +0800

    - reachability

--- PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
@@ -1,19 +1,25 @@
-import UIKit
+import Foundation
 import PlaygroundSupport
 
-let ss = "202,203,206,1"
+PlaygroundPage.current.needsIndefiniteExecution = true
 
- func getColorFromRGBStr(_ colorStr: String?) -> UIColor? {
-    guard colorStr != nil else {
-        return nil
-    }
-    let tArrSplited = colorStr?.components(separatedBy: ",")
-    guard let tArr = tArrSplited, tArr.count == 4 else {
-        return nil
-    }
-    let tArrFloat = tArr.map{  CGFloat(( $0 as NSString).floatValue)}
+if let url = URL(string: "https://apple.com") {
+    var request = URLRequest(url: url)
+    request.httpMethod = "HEAD"
+    URLSession(configuration: .default)
+        .dataTask(with: request) { (_, response, error) -> Void in
+            guard error == nil else {
+                print("Error:", error ?? "")
+                return
+            }
 
-    return UIColor(red: (tArrFloat[0]) / 255.0, green: (tArrFloat[1]) / 255.0, blue: (tArrFloat[2]) / 255.0, alpha: tArrFloat[3])
+            guard (response as? HTTPURLResponse)?
+                .statusCode == 200 else {
+                    print("down")
+                    return
+            }
+
+            print("up")
+        }
+        .resume()
 }
-
-getColorFromRGBStr(ss)
