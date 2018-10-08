ff3c5fb8c50f363c589446be39df991189469366
certainly <j1o2b3@gmail.com>
2018-10-08 16:07:22 +0800

    - oxford lookup

--- PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
@@ -1,25 +1,35 @@
 import Foundation
 import PlaygroundSupport
 
+// TODO: replace with your own app id and app key
+
 PlaygroundPage.current.needsIndefiniteExecution = true
+let appId = "18f13eaa"
+let appKey = "27a056e3a3982744b83c21ea4f30578d"
+let language = "en"
+let word = "Ace"
+let word_id = word.lowercased() //word id is case sensitive and lowercase is required
+let url = URL(string: "https://od-api.oxforddictionaries.com:443/api/v1/entries/\(language)/\(word_id)")!
+var request = URLRequest(url: url)
+request.addValue("application/json", forHTTPHeaderField: "Accept")
+request.addValue(appId, forHTTPHeaderField: "app_id")
+request.addValue(appKey, forHTTPHeaderField: "app_key")
 
-if let url = URL(string: "https://apple.com") {
-    var request = URLRequest(url: url)
-    request.httpMethod = "HEAD"
-    URLSession(configuration: .default)
-        .dataTask(with: request) { (_, response, error) -> Void in
-            guard error == nil else {
-                print("Error:", error ?? "")
-                return
-            }
-
-            guard (response as? HTTPURLResponse)?
-                .statusCode == 200 else {
-                    print("down")
-                    return
-            }
-
-            print("up")
+let session = URLSession.shared
+_ = session.dataTask(with: request, completionHandler: { data, response, error in
+    if let response = response,
+        let data = data,
+        let jsonData = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) {
+        if let JSONString = String(data: data, encoding: String.Encoding.utf8) {
+            print(JSONString)
         }
-        .resume()
-}
+//        print(response)
+//        print(jsonData)
+    } else {
+        print(error)
+        print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue))
+    }
+    PlaygroundPage.current.finishExecution()
+}).resume()
+
+
