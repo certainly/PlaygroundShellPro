0995cc6840a6edad7179e7cd0191c5e8608e09c3
certainly <j1o2b3@gmail.com>
2019-03-27 09:44:23 +0800

    - urlsession

--- PlaygroundShellPro/MyPlayground.playground/Pages/testHere.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/testHere.xcplaygroundpage/Contents.swift
@@ -1,7 +1,15 @@
 //: [Previous](@previous)
 
 import Foundation
-let code = "zh"
-var str = "<option value=\"\(code)\">"
+import PlaygroundSupport
 
-//: [Next](@next)
+let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+//let url = URL(string: "http://api.plos.org/search?q=title:DNA")!
+
+let dataTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
+    let json = try? JSONSerialization.jsonObject(with: data ?? Data(), options: [])
+    print(json ?? "n/a")
+    PlaygroundPage.current.finishExecution()
+}
+
+dataTask.resume()
