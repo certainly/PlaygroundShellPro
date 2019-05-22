5ae955b0421abf3a1ae535a9acef23a27d9b0881
certainly <j1o2b3@gmail.com>
2019-05-23 07:39:39 +0800

    - http demo

--- PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
@@ -2,13 +2,44 @@ import Foundation
 import PlaygroundSupport
 import UIKit
 
-let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+print("67jkj07")
+//#warning("todo: updatejj")
 
-let dataTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
-    let json = try? JSONSerialization.jsonObject(with: data ?? Data(), options: [])
-    print(json ?? "n/a")
 
+
+////////////////////////////////
+//PlaygroundPage.current.needsIndefiniteExecution = true
+func parse()  {
+    var urlComponents = URLComponents()
+    urlComponents.scheme = "http"
+    urlComponents.host = "certainly.cf"
+    urlComponents.port = 8001
+    guard let url = urlComponents.url else { fatalError("Could not create URL from components") }
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+
+    let config = URLSessionConfiguration.default
+    let session = URLSession(configuration: config)
+    let task = session.dataTask(with: request) { (responseData, response, responseError) in
+        print("response ...")
+        guard responseError == nil else {
+            print("fail")
+            print(responseError?.localizedDescription)
+            return
+        }
+
+        guard let jsonData = responseData else {
+            let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Data was not retrieved from request"]) as Error
+            print("succ")
+            return
+        }
+
+        // print string
+        if let returnData = String(data: jsonData, encoding: .utf8) {
+                        print("jsondata: \(returnData)")
+        }
+    }
+    task.resume()
 }
 
-dataTask.resume()
-
+parse()
