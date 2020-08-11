53fcdcd983151e387faac997de6954f637a4ea6d
certainly <j1o2b3@gmail.com>
2020-08-11 18:05:24 +0800

    + P1: Working with an Internet Connection on iOS with Swift: Best Practices

--- PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/now.xcplaygroundpage/Contents.swift
@@ -2,34 +2,27 @@ import Foundation
 import PlaygroundSupport
 import UIKit
 
+//import os
+
+import os.log
+
 print("67jk444j07")
 //#warning("todo: updatejj")
 
 
 
+
 ////////////////////////////////
-//PlaygroundPage.current.needsIndefiniteExecution = true
 
-let url = URL(string: "https://api.github.com/repos/johnsundell/publish")!
-let publisher = URLSession.shared.dataTaskPublisher(for: url)
 
-let repoPublisher = publisher
-    .map(\.data)
-    .decode(type: Repository.self, decoder: JSONDecoder())
+let config = URLSessionConfiguration.default
+config.waitsForConnectivity = true
+config.timeoutIntervalForResource = 300
 
-let cancellable = repoPublisher.sink(receiveCompletion: { (completion) in
-    switch completion {
-    case .failure(let error):
-        print(error)
-    case .finished:
-        print("Success")
+let session = URLSession(configuration: config)
 
-    }
-}) { repo in
-    print(repo)
-}
+let url = URL(string: "https://www.example.com/")!
 
-struct Repository: Codable {
-    var name: String
-    var url: URL
+session.dataTask(with: url) { data, response, error in
+    // ...
 }
