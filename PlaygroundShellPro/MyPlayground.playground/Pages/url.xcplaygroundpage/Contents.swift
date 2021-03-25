8ebd580547c49eba4493f3fd91da471d178848e7
certainly <j1o2b3@gmail.com>
2021-03-25 20:09:10 +0800

    ! rest api architecture

--- PlaygroundShellPro/MyPlayground.playground/Pages/url.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/url.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 import Foundation
 import PlaygroundSupport
-import UIKit
+//import UIKit
 
 print("67jkj07")
 //#warning("todo: updatejj")
@@ -8,15 +8,131 @@ print("67jkj07")
 
 
 ////////////////////////////////
-let url = URL(string: "https://api.chucknorris.io/jokes/random")!
 
-let dataTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
-    if let data = data,  let str = String(data: data, encoding: .utf8) {
-        print("str: \(str)")
+//func loadQuestion() {
+//
+//    let resource = QuestionsResource(id: 60043628)
+//    let request = APIRequest(resource: resource)
+//    self.questionRequest = request
+//    request.execute { [weak self] questions in
+//        guard let question = questions?.first else { return }
+//
+//    }
+//}
+var aRequest : StringRequest?
+private var jokeRequest: APIRequest<JokeResource>?
+func loadString() {
+    let url = URL(string: "https://api.chucknorris.io/jokes/random")!
+//    let url = URL(string: "https://www.starwars.com")!
+//    let url = URL(string: "https://www.baidu.com")!
+    aRequest = StringRequest(url: url)
+    aRequest?.execute { str in
+        print("String: \(str)")
+        exit(0)
     }
-//    let json = try? JSONSerialization.jsonObject(with: data ?? Data(), options: [])
-//    print(json ?? "n/a")
 
 }
 
-dataTask.resume()
+func loadJoke() {
+    let resource = JokeResource()
+    let request = APIRequest(resource: resource)
+    jokeRequest = request
+    jokeRequest?.execute { data in
+        print("data: \(data)")
+        exit(0)
+    }
+
+}
+
+//loadJoke()
+loadString()
+
+
+//if true { exit(0) }
+//let url = URL(string: "https://api.chucknorris.io/jokes/random")!
+let url = URL(string: "https://justmysocks3.net/members/getbwcounter.php?service=62051&id=a3ba05f3-ba90-4e7b-83c2-48fad6fbefbc")!
+//let url = URL(string: "https://www.baidu.com")!
+
+//let cancellable = URLSession.shared.dataTaskPublisher(for: url)
+////    .map{ String(data: $0.data, encoding: .utf8) }
+//    .map (\.data)
+////    .compactMap {$0}
+//    .decode(type: Counter.self, decoder: JSONDecoder())
+//    .sink { completion in
+////        print(completion) // finished
+//    } receiveValue: { counter in
+//        print(counter)
+//        //        if let str = String(data: place, encoding: .utf8) {
+//        //            print(str)
+//        //        }
+//        //        print("\(ByteCountFormatter().string(fromByteCount: Int64(place.bw_counter_b)))")
+//        let byteFormatter = ByteCountFormatter()
+////        byteFormatter.countStyle = .binary
+////        byteFormatter.includesActualByteCount = true
+////        byteFormatter.includesUnit = false
+//        let out = byteFormatter.string(fromByteCount: Int64(counter.bw_counter_b))
+//        let out2 = byteFormatter.string(fromByteCount: Int64(100_000_000_100 - 263894665))
+//        print(out)
+//        exit(EXIT_SUCCESS)
+//    }
+//
+////URLSession.shared.dataTask(with: url) { (data, _, error) in
+////    if let data = data,  let str = String(data: data, encoding: .utf8) {
+////        print("str: \(str)")
+////        if let rz = try? JSONDecoder().decode(Counter.self, from: data ).bw_counter_b {
+////            let out = ByteCountFormatter().string(fromByteCount: Int64(rz))
+////            print("rz: \(rz)")
+////            print("out: \(out)")
+////        }
+////
+////    } else if error != nil {
+////        print(error?.localizedDescription ?? "error..")
+////    }
+////}.resume()
+//
+////let cancellable = URLSession.shared.dataTaskPublisher(for: url)
+//////    .map (\.data)
+////    .map{ String(data: $0.data, encoding: .utf8) }
+////    .compactMap {$0}
+//////    .decode(type: Counter.self, decoder: JSONDecoder())
+//////    .receive(on: RunLoop.main)
+//////    //.replaceError(with: [])
+//////    .eraseToAnyPublisher()÷
+////    .sink(receiveCompletion: { completion in
+//////        print(completion) // finished
+////    }) { place in
+////        print(place)
+//////        if let str = String(data: place, encoding: .utf8) {
+//////            print(str)
+//////        }
+//////        print("\(ByteCountFormatter().string(fromByteCount: Int64(place.bw_counter_b)))")
+////        exit(EXIT_SUCCESS)
+////    }
+//
+//
+//
+//struct Counter: Codable {
+//    var monthly_bw_limit_b: Int
+//    var bw_counter_b: Int
+//    var bw_reset_day_of_month: Int
+//}
+
+
+
+//let url = URL(string: "https://rss.itunes.apple.com/api/v1/US/apple-music/hot-tracks/all/50/explicit.json")!
+//
+//let cancellable = URLSession.shared.dataTaskPublisher(for: url)
+//    .map(\.data)
+//    .decode(type: Feed.self, decoder: JSONDecoder())
+//    .sink { completion in
+////        print(completion) // finished
+//    } receiveValue: { counter in
+//        print(counter)
+//        //        if let str = String(data: place, encoding: .utf8) {
+//        //            print(str)
+//        //        }
+//
+//
+//
+//        exit(EXIT_SUCCESS)
+//    }
---
+++ PlaygroundShellPro/MyPlayground.playground/Pages/url.xcplaygroundpage/Sources/NetworkRequest.swift
@@ -0,0 +1,156 @@
+//
+//  NetworkRequest.swift
+//  TopQuestion
+//
+//  Created by Matteo Manferdini on 12/09/2019.
+//  Copyright © 2019 Matteo Manferdini. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: - NetworkRequest
+public protocol NetworkRequest: AnyObject {
+	associatedtype ModelType
+    func decode(_ data: Data) -> ModelType?
+    func execute(withCompletion completion: @escaping (ModelType?) -> Void)
+}
+
+extension NetworkRequest {
+	fileprivate func load(_ url: URL, withCompletion completion: @escaping (ModelType?) -> Void) {
+		let task = URLSession.shared.dataTask(with: url) { [weak self] (data, _ , _) -> Void in
+
+//            print("333")
+//            print(self)
+//            if let data = data {
+//                print(String(data: data, encoding: .utf8))
+//                print(data.count)
+//                let decoder = JSONDecoder()
+//                let tt = try? decoder.decode(Joke.self, from: data)
+//                print("az \(tt)")
+//            }
+//            print("3434")
+			guard let data = data, let value = self?.decode(data) else {
+				DispatchQueue.main.async { completion(nil) }
+				return
+			}
+			 completion(value) 
+		}
+		task.resume()
+	}
+}
+
+
+
+// MARK: - StringRequest
+public class StringRequest {
+    public let url: URL
+    public init(url: URL) {
+        self.url = url
+    }
+}
+
+extension StringRequest: NetworkRequest {
+    public func execute(withCompletion completion: @escaping (String?) -> Void) {
+        load(url, withCompletion: completion)
+    }
+
+    public func decode(_ data: Data) -> String? {
+        return  String(data: data, encoding: .utf8)
+    }
+}
+
+// MARK: - APIRequest
+public class APIRequest<Resource: APIResource> {
+	let resource: Resource
+	
+	public init(resource: Resource) {
+		self.resource = resource
+	}
+}
+
+extension APIRequest: NetworkRequest {
+    public func decode(_ data: Data) -> Resource.ModelType?{
+		let decoder = JSONDecoder()
+		decoder.dateDecodingStrategy = .secondsSince1970
+		let wrapper = try? decoder.decode(Resource.ModelType.self, from: data)
+        return wrapper
+	}
+	
+    public func execute(withCompletion completion: @escaping (Resource.ModelType?) -> Void) {
+		load(resource.url, withCompletion: completion)
+	}
+}
+
+// MARK: - APIResource
+public protocol APIResource {
+	associatedtype ModelType: Decodable
+	var methodPath: String { get }
+	var filter: String? { get }
+    var url: URL { get }
+}
+
+extension APIResource {
+
+//	var url: URL {
+//		var components = URLComponents(string: "https://api.stackexchange.com/2.2")!
+//		components.path = methodPath
+//		components.queryItems = [
+//			URLQueryItem(name: "site", value: "stackoverflow"),
+//			URLQueryItem(name: "order", value: "desc"),
+//			URLQueryItem(name: "sort", value: "votes"),
+//			URLQueryItem(name: "tagged", value: "swiftui"),
+//			URLQueryItem(name: "pagesize", value: "10")
+//		]
+//		if let filter = filter {
+//			components.queryItems?.append(URLQueryItem(name: "filter", value: filter))
+//		}
+//		return components.url!
+//	}
+}
+
+// MARK: - Wrapper
+public struct Wrapper<T: Decodable>: Decodable {
+    let items: [T]
+}
+
+// MARK: - JokeResource
+public struct JokeResource: APIResource {
+    public var url: URL = URL(string: "https://api.chucknorris.io/jokes/random")!
+
+    public var methodPath: String = ""
+ 
+    public var filter: String?
+
+    public typealias ModelType = Joke
+
+
+    public init() {
+
+    }
+
+}
+
+public struct Joke: Decodable {
+    public let url: String?
+    public let id: String?
+}
+
+// MARK: - QuestionsResource
+//public struct QuestionsResource: APIResource {
+//    public typealias ModelType = Question
+//	public var id: Int?
+//
+//
+//
+//    public var methodPath: String {
+//		guard let id = id else {
+//			return "/questions"
+//		}
+//		return "/questions/\(id)"
+//	}
+//
+//    public var filter: String? {
+//		id != nil ? "!9_bDDxJY5" : nil
+//	}
+//}
