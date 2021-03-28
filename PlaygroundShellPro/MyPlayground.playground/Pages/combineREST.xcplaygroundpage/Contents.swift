c9f0939592f6085496006c8ced4c3f8f887851bf
certainly <j1o2b3@gmail.com>
2021-03-28 20:13:16 +0800

    ! pretty json

--- PlaygroundShellPro/MyPlayground.playground/Pages/combineREST.xcplaygroundpage/Contents.swift
+++ PlaygroundShellPro/MyPlayground.playground/Pages/combineREST.xcplaygroundpage/Contents.swift
@@ -5,23 +5,40 @@ import Combine
 
 print("dfdf")
 
-//var cancellables = Set<AnyCancellable>()
 
-class MyClass {
-    var rz: String = ""
-}
+//let urlString = "https://api.chucknorris.io/jokes/random"
+//let urlString = "https://rss.itunes.apple.com/api/v1/us/apple-music/hot-tracks/all/50/explicit.json"
+let urlString = "https://justmysocks3.net/members/getbwcounter.php?service=62051&id=a3ba05f3-ba90-4e7b-83c2-48fad6fbefbc"
+let url = URL(string: urlString)!
 
-let url = URL(string: "https://api.chucknorris.io/jokes/random")!
-var myObject = MyClass()
 var cancellables = URLSession.shared
     .dataTaskPublisher(for: url)
-//    .map(\.data)
-    .map{ String(data: $0.data, encoding: .utf8) }
-//    .replaceError(with: [])
-//    .receive(on: RunLoop.main)
+    .map(\.data)
+    .map(\.prettyJson!)
+//    .map { String(decoding: $0, as: UTF8.self) }
+    
+
+//    .decode(type: Counter.self, decoder: JSONDecoder())
+
     .sink(receiveCompletion: { completion in
 
        }) { post in
-           print(post!)
+           print(post)
        }
 //    .store(in: &cancellables)
+
+struct Counter: Codable {
+    var monthly_bw_limit_b: Int
+    var bw_counter_b: Int
+    var bw_reset_day_of_month: Int
+}
+
+extension Data {
+    var prettyJson: String? {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = String(data: data, encoding:.utf8) else { return nil }
+
+        return prettyPrintedString
+    }
+}
